5aca779ada3ab9f9a2464bb998249d88b924c60b
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2018-06-19 13:12:36 +0930

    Added iOS class library for rendering the expandable list on iOS.

---
+++ SharedSpace.iOS.Lib/ExpandableListViewEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SharedSpace.iOS.Lib
+{
+	public class ExpandableListViewEventArgs : PropertyChangedEventArgs
+	{
+		public ExpandableListViewEventArgs(string propertyName) : base(propertyName)
+		{
+		}
+
+		public int GroupPosition { get; set; }
+		public int ChildPosition { get; set; }
+	}
+}
