70784a2543c34d3641801924c78c09ceae2ac0f2
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2018-06-20 11:52:16 +0930

    Changes to Core SharedSpace

--- SharedSpace/Features/MainPageModel.cs
+++ SharedSpace/Features/MainPageModel.cs
@@ -1,8 +1,6 @@
 ﻿using SharedSpace.DomainObjects;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace SharedSpace.Features
